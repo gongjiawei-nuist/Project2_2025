97444e2f62cdc97e01f8b233bb82d9414b4a2085
michael <coolcom5@gmail.com>
2025-04-22 14:23:47 +0100

    change code

--- SoilSensorEmail.py
+++ SoilSensorEmail.py
@@ -8,28 +8,23 @@ GPIO.setup(channel,GPIO.IN)
 from_email_addr="bno03972367@163.com"
 from_email_pass="GTNMYJAJP8h2ghT5"
 to_email_addr="bno03972365@163.com"
-daily_reading=[]
-def callback(channel):
-        if not GPIO.input(channel)
-		send_daily_report()
-GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
-GPIO.add_event_callback(channel,callback)
-def send_daily_report():
+daily_readings=[]
+def send_email(moisture_status):
 	msg=EmailMessage()
-	body="Daily Moisture Report"
+	body=f"Soil moisture: {moisture_status}"
 	msg.set_content(body)
 	msg['From']=from_email_addr
 	msg['To']=to_email_addr
-	msg['Subject']='TEST EMAIL'
+	msg['Subject']='moisture status report'
 	server=smtplib.SMTP('smtp.163.com',25)
 	server.starttls()
 	server.login(from_email_addr,from_email_pass)
 	server.send_message(msg)
 	server.quit()
-while True:
-        reading =GPIO.input(channel)
-	daily_readings.append(reading)
-	time.sleep(3600)
-	if len(daily_readings)==4
-		send_daily_report()
-		daily_readings=[]
+try:
+	while True:
+		moisture_status ="dry"if GPIO.input(channel)else "wet"
+		send_email(moisture_status)
+		time.sleep(60)
+except KeyboardInterrupt:
+	GPIO.cleanup()
